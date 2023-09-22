17d97d79b8af397ae1fa33034a6762c94feaf303
crisdev-ec <desarrollotech.ec@hotmail.com>
2023-09-22 10:22:15 -0500

    seen topic: const, var, let

--- 3-Reforzamiento-temas-js/const-var-let.js
+++ 3-Reforzamiento-temas-js/const-var-let.js
@@ -1,3 +1,13 @@
+/* VAR -> Crea una variable en el ámbito global
+             “No siempre es lo que necesito”
+
+var nombre = 'Wolverine';
+ if (true){
+    var nombre = 'Magneto';
+ }
+console.log(nombre);
+
+*/
 
 let nombre = 'Wolverine';
 
